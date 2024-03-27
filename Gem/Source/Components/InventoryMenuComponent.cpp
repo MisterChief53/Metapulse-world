cc5dff2663ed98d951b00df54e48d9e1da309ebf
MisterChief53 <soderol3@hotmail.com>
2024-03-27 12:34:47 -0600

    added basic one-way dragging in inventory menu

--- Gem/Source/Components/InventoryMenuComponent.cpp
+++ Gem/Source/Components/InventoryMenuComponent.cpp
@@ -4,6 +4,9 @@
 #include <LyShine/Bus/UiButtonBus.h>
 #include <AzCore/Console/ILogger.h>
 #include <LyShine/Bus/UiCanvasManagerBus.h>
+#include <LyShine/Bus/UiDraggableBus.h>
+#include <LyShine/Bus/UiCanvasBus.h>
+#include <LyShine/Bus/UiElementBus.h>
 
 namespace metapulseWorld {
 	void InventoryMenuComponent::Init()
@@ -23,6 +26,8 @@ namespace metapulseWorld {
 				AZLOG_INFO("unloading canvas");
 				UiCanvasManagerBus::Broadcast(&UiCanvasManagerBus::Events::UnloadCanvas, canvasEntityId);
 			});
+
+		UiDropTargetNotificationBus::Handler::BusConnect(m_equippedDropTargetEntityId);
 	}
 	void InventoryMenuComponent::Deactivate()
 	{
@@ -33,6 +38,9 @@ namespace metapulseWorld {
 			serializeContext->Class<InventoryMenuComponent, AZ::Component>()
 				->Version(1)
 				->Field("Close Button Entity", &InventoryMenuComponent::m_closeButtonEntityId)
+				->Field("Unequipped List Entity", &InventoryMenuComponent::m_unequippedItemsListEntityId)
+				->Field("Equipped List Entity", &InventoryMenuComponent::m_equippedItemsListEntityId)
+				->Field("Equipped Drop Target", &InventoryMenuComponent::m_equippedDropTargetEntityId)
 				;
 
 			if (AZ::EditContext* editContext = serializeContext->GetEditContext()) {
@@ -42,8 +50,30 @@ namespace metapulseWorld {
 					->Attribute(AZ::Edit::Attributes::Icon, "Icons/Components/Component_Placeholder.svg")
 					->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("CanvasUI"))
 					->DataElement(AZ::Edit::UIHandlers::Default, &InventoryMenuComponent::m_closeButtonEntityId, "Close Button Entity Id", "The id of the button used to close this UI")
+					->DataElement(AZ::Edit::UIHandlers::Default, &InventoryMenuComponent::m_unequippedItemsListEntityId, "Unequipped List Entity", "The id of the list that contains unequipped items")
+					->DataElement(AZ::Edit::UIHandlers::Default, &InventoryMenuComponent::m_equippedItemsListEntityId, "Equipped List Entity", "The id of the list that contains equipped items")
+					->DataElement(AZ::Edit::UIHandlers::Default, &InventoryMenuComponent::m_equippedDropTargetEntityId, "Equipped Drop Target", "The id of the drop target for equipped items")
 					;
 			}
+		}
+	}
+	void InventoryMenuComponent::OnDropHoverStart([[maybe_unused]] AZ::EntityId draggable)
+	{
+	}
+	void InventoryMenuComponent::OnDropHoverEnd([[maybe_unused]] AZ::EntityId draggable)
+	{
+	}
+	void InventoryMenuComponent::OnDrop(AZ::EntityId draggable)
+	{
+		AZ::EntityId draggableParent;
+		UiElementBus::EventResult(draggableParent, draggable, &UiElementBus::Events::GetParentEntityId);
+
+		if (draggableParent.IsValid() && draggableParent == m_unequippedItemsListEntityId) {
+			// 0 to assign an invalid entityid
+			UiElementBus::Event(draggable, &UiElementBus::Events::ReparentByEntityId, m_equippedItemsListEntityId, AZ::EntityId());
+		}
+		else if (draggableParent.IsValid() && draggableParent == m_equippedItemsListEntityId) {
+			UiElementBus::Event(draggable, &UiElementBus::Events::ReparentByEntityId, m_unequippedItemsListEntityId, AZ::EntityId());
 		}
 	}
 }
--- Gem/Source/Components/InventoryMenuComponent.h
+++ Gem/Source/Components/InventoryMenuComponent.h
@@ -1,8 +1,10 @@
 #include <AzCore/Component/Component.h>
+#include <LyShine/Bus/UiDropTargetBus.h>
 
 namespace metapulseWorld {
 	class InventoryMenuComponent
-		: public AZ::Component {
+		: public AZ::Component
+		, public UiDropTargetNotificationBus::Handler {
 	public:
 		AZ_COMPONENT(metapulseWorld::InventoryMenuComponent, "{9EAAE2FF-9265-4BA0-98EE-ADACEAE57911}", AZ::Component);
 
@@ -12,7 +14,15 @@ namespace metapulseWorld {
 		void Deactivate() override;
 		static void Reflect(AZ::ReflectContext* context);
 
+		// Drop target overrides
+		void OnDropHoverStart(AZ::EntityId draggable) override;
+		void OnDropHoverEnd(AZ::EntityId draggable) override;
+		void OnDrop(AZ::EntityId draggable) override;
+
 	private:
 		AZ::EntityId m_closeButtonEntityId;
+		AZ::EntityId m_unequippedItemsListEntityId;
+		AZ::EntityId m_equippedItemsListEntityId;
+		AZ::EntityId m_equippedDropTargetEntityId;
 	};
 }
